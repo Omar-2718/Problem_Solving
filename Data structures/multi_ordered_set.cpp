204affda5944ec6585dc4f82c128b041ce88edae
Omar-2718 <omarmsayed2718@gmail.com>
2025-08-01 01:33:10 +0300

    Fixed ordered set

--- Data structures/multi_ordered_set.cpp
+++ Data structures/multi_ordered_set.cpp
@@ -1,4 +1,8 @@
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-typedef treelong long,null_type,less_equallong long,rb_tree_tag,tree_order_statistics_node_update ordered_set;
-find_by_order() The first returns an iterator to the k-th largest element (counting from zero)
-order_of_key() the number of items in a set that are strictly smaller than our item
+typedef tree<long long,null_type,less_equal<long long>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+//find_by_order() The first returns an iterator to the k-th largest element (counting from zero)
+//order_of_key() the number of items in a set that are strictly smaller than our item
+
